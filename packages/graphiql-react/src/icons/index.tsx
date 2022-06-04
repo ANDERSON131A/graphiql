647e69676872235da09d63ded9551f92d3271528
Thomas Heyenbrock <thomas.heyenbrock@gmail.com>
2022-06-04 14:58:48 +0200

    add icons

--- packages/graphiql-react/src/icons/index.tsx
+++ packages/graphiql-react/src/icons/index.tsx
@@ -1,25 +1,46 @@
+import _ArgumentIcon from './argument.svg';
 import _ChevronDownIcon from './chevron-down.svg';
+import _ChevronLeftIcon from './chevron-left.svg';
 import _ChevronUpIcon from './chevron-up.svg';
 import _CopyIcon from './copy.svg';
+import _DeprecatedArgumentIcon from './deprecated-argument.svg';
+import _DeprecatedFieldIcon from './deprecated-field.svg';
 import _DocsIcon from './docs.svg';
+import _FieldIcon from './field.svg';
 import _HistoryIcon from './history.svg';
 import _KeyboardShortcutIcon from './keyboard-shortcut.svg';
 import _MergeIcon from './merge.svg';
 import _PenIcon from './pen.svg';
 import _PlayIcon from './play.svg';
 import _PrettifyIcon from './prettify.svg';
+import _RootTypeIcon from './root-type.svg';
 import _SettingsIcon from './settings.svg';
 import _StarFilledIcon from './star-filled.svg';
 import _StarIcon from './star.svg';
 import _StopIcon from './stop.svg';
+import _TypeIcon from './type.svg';
 
+export const ArgumentIcon = generateIcon(_ArgumentIcon, 'argument icon');
 export const ChevronDownIcon = generateIcon(
   _ChevronDownIcon,
   'chevron down icon',
 );
+export const ChevronLeftIcon = generateIcon(
+  _ChevronLeftIcon,
+  'chevron left icon',
+);
 export const ChevronUpIcon = generateIcon(_ChevronUpIcon, 'chevron up icon');
 export const CopyIcon = generateIcon(_CopyIcon, 'copy icon');
+export const DeprecatedArgumentIcon = generateIcon(
+  _DeprecatedArgumentIcon,
+  'deprecated argument icon',
+);
+export const DeprecatedFieldIcon = generateIcon(
+  _DeprecatedFieldIcon,
+  'deprecated field icon',
+);
 export const DocsIcon = generateIcon(_DocsIcon, 'docs icon');
+export const FieldIcon = generateIcon(_FieldIcon, 'field icon');
 export const HistoryIcon = generateIcon(_HistoryIcon, 'history icon');
 export const KeyboardShortcutIcon = generateIcon(
   _KeyboardShortcutIcon,
@@ -29,10 +50,12 @@ export const MergeIcon = generateIcon(_MergeIcon, 'merge icon');
 export const PenIcon = generateIcon(_PenIcon, 'pen icon');
 export const PlayIcon = generateIcon(_PlayIcon, 'play icon');
 export const PrettifyIcon = generateIcon(_PrettifyIcon, 'prettify icon');
+export const RootTypeIcon = generateIcon(_RootTypeIcon, 'root type icon');
 export const SettingsIcon = generateIcon(_SettingsIcon, 'settings icon');
 export const StarFilledIcon = generateIcon(_StarFilledIcon, 'filled star icon');
 export const StarIcon = generateIcon(_StarIcon, 'star icon');
 export const StopIcon = generateIcon(_StopIcon, 'stop icon');
+export const TypeIcon = generateIcon(_TypeIcon, 'type icon');
 
 function generateIcon(RawComponent: any, title: string) {
   const WithTitle = function IconComponent(
